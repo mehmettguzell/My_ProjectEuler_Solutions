d343ed26b62e8810795f0a3fe2fc7b1201f63112
mehmettguzell <>
2025-11-16 15:28:00 +0300

    day3

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # HackerRank ProjectEuler+ Solutions
 
-This repository contains my solutions to the Project Euler+ challenges on HackerRank.  
-The goal is to improve my problem-solving and algorithmic thinking by practicing daily.
+- This repository contains my solutions to the Project Euler+ challenges on HackerRank.  
+- The goal is to improve my problem-solving and algorithmic thinking by practicing daily.
 
 🔗 Challenge page: https://www.hackerrank.com/contests/projecteuler/challenges
 
--- euler3.java
+++ euler3.java
@@ -1,7 +1,47 @@
 package My_ProjectEuler_Solutions;
 
-public class euler3 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
 
+public class euler3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+                //code here
+                System.out.println(largestPrimeFactor(n));
+
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+
+    private static long largestPrimeFactor(long n) {
+        long maxPrime = -1;
+
+        while (n % 2 == 0) {
+            maxPrime = 2;
+            n /= 2;
+        }
+
+        for (long i = 3; i * i <= n; i += 2) {
+            while (n % i == 0) {
+                maxPrime = i;
+                n /= i;
+            }
+        }
+
+        if (n > 2) maxPrime = n;
+
+        return maxPrime;
     }
 }
